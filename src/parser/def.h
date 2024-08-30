5e2da8874edc46d9ac7da1d5f9f63cd53c5d5dc2
wayne-ziqi <972282146@qq.com>
2024-08-30 11:37:34 +0800

    update: copy right

--- src/parser/def.h
+++ src/parser/def.h
@@ -22,6 +22,16 @@
 #ifndef WSDB_DEF_H
 #define WSDB_DEF_H
 
+/* Copyright (c) 2023 Renmin University of China
+RMDB is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+        http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details. */
+
 int yyparse();
 
 typedef struct yy_buffer_state *YY_BUFFER_STATE;
